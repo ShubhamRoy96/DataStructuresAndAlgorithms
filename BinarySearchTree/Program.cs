14b8c48bd3c315488777ddaa9c2ad93d1edda3dc
Shubham Roy <shubhamroy896@gmail.com>
2022-05-04 10:41:24 +0530

    Implement BST

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,21 +6,70 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinarySearchTree tree = new BinarySearchTree();
+            tree.insert(8);
+            tree.insert(3);
+            tree.insert(19);
+            tree.insert(14);
+            tree.insert(169);
+            tree.insert(50);
         }
     }
 
     class Node
     {
-        int _left;
-        int _right;
-        int _value;
+        public Node Left { get; set; }
+        public Node Right{ get; set; }
+        public int Value { get; set; }
 
-        public Node (int left, int right, int value)
+        public Node(int value)
         {
-            _left = left;
-            _right = right;
-            _value = value;
+            Left = null;
+            Right = null;
+            Value = value;
+        }
+    }
+
+    class BinarySearchTree
+    {
+        Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void insert(int nodeValue)
+        {
+            var newNode = new Node(nodeValue);
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                var currentNode = root;
+                while (true)
+                {
+                    if(nodeValue < currentNode.Value) //left
+                    {
+                        if(currentNode.Left == null)
+                        {
+                            currentNode.Left = newNode;
+                            return;
+                        }
+                        currentNode = currentNode.Left;
+                    }
+                    else
+                    {
+                        if(currentNode.Right == null)
+                        {
+                            currentNode.Right = newNode;
+                            return;
+                        }
+                        currentNode = currentNode.Right;
+                    }
+                }
+            }
         }
     }
 }
